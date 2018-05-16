61964101221c9fcf35f6aa52b28e131654073c75
Ivan Malinovsky <swaveful@gmail.com>
2018-05-16 19:04:17 +0300

    added domains for popular words

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 var axios = require('axios');
-var lineArr;
-var domainEndWords = [];
-
-getData('domain_zones.txt');
+var domainEndOtherWords = [];
+var domainZones = [];
+var wordsArray = [];
+var domainEndPopularWords = [];
 var urlForDomains = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt';
-
+getPopularWords();
+getData('domain_zones.txt');
 axios({
         method: 'get',
         url: urlForDomains,
@@ -23,60 +24,58 @@ function getData() { //this will read file and send information to other functio
             throw err;
         }
         console.log('Loaded domain zones!');
-        intoArray(data);
+        domainZones = data.split('\n');
     });
 }
 
-function intoArray(lines) {
-    lineArr = lines.split('\n');
-}
 
-function checkForDomainEnd(wordsArray) {
-    for (var i = 0; i < (lineArr.length - 1); i++) {
+function checkForDomainEnd(wordsArray, finalDomains) {
+    for (var i = 0; i < (domainZones.length - 1); i++) {
         for (var j = 0; j < wordsArray.length; j++) {
-            if (wordsArray[j].toLowerCase().endsWith(lineArr[i].toLowerCase()) && ((wordsArray[j].length - lineArr[i].length) > 0)) {
-                var wordWithDot = (wordsArray[j].substr(0, wordsArray[j].length - lineArr[i].length) + "." + lineArr[i]).toLowerCase();
-                domainEndWords.push(wordWithDot);
+            if (wordsArray[j].toLowerCase().endsWith(domainZones[i].toLowerCase()) && ((wordsArray[j].length - domainZones[i].length) > 0)) {
+                var wordWithDot = (wordsArray[j].substr(0, wordsArray[j].length - domainZones[i].length) + "." + domainZones[i]).toLowerCase();
+                finalDomains.push(wordWithDot);
             }
         }
     }
-    
+
     console.log('Domain words are');
-    for (i = 0; i < domainEndWords.length; i++) {
-        console.log(domainEndWords[i]);
+    for (i = 0; i < finalDomains.length; i++) {
+        console.log(finalDomains[i]);
     }
-    console.log('Found ' + domainEndWords.length + ' domains');
+    console.log('Found ' + finalDomains.length + ' domains');
 }
 
 function dataAjax(data) {
     console.log("Loaded vocabulary!");
-    console.log("Computing data...");
-    var wordsArray = data.split("\n");
-    checkForDomainEnd(wordsArray);
-    writeInFile();
+    wordsArray = data.split("\n");
+    console.log('Computing data for popular words');
+    checkForDomainEnd(popularWords, domainEndPopularWords);
+    writeInFile(domainEndPopularWords, 'popular_words_domains.txt');
+    /*console.log("Computing data for other words...");
+    checkForDomainEnd(wordsArray, domainEndOtherWords);
+    writeInFile(domainEndOtherWords, "domains.txt");*/
 }
-function writeInFile(){
+
+function writeInFile(finalDomains, domainsFile) {
     var fs = require('fs');
-var stream = fs.createWriteStream("domains.txt");
-stream.once('open', function(fd) {
-    for (i = 0; i < domainEndWords.length; i++) {
-        stream.write(domainEndWords[i] + " \n");
-    }
-  stream.end();
-});
+    var stream = fs.createWriteStream(domainsFile);
+    stream.once('open', function (fd) {
+        for (i = 0; i < finalDomains.length; i++) {
+            stream.write(finalDomains[i] + " \n");
+        }
+        stream.end();
+    });
 }
 
+function getPopularWords() { //this will read file and send information to other function
+    var fs = require('fs');
 
-
-/*
-var urlTopWords = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt';
-axios({
-    method: 'get',
-    url: urlTopWords,
-    responseType: 'text'
-})
-.then(function(response){
-    console.info(Object.getOwnPropertyNames(response));
-    var mostFrequentwords = intoArray(response.data);
-    console.log('most frequent word is' + mostFrequentwords[0]);
-}); */
+    fs.readFile('app/10000_popular_words.txt', 'utf8', function (err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log('Loaded popular words!');
+        popularWords = data.split('\n');
+    });
+}
